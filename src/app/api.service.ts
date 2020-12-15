315ad51f5706ac2d6808bf503e6e551f0e37d2e0
Scrambles86 <craigvidler86@outlook.com>
2020-12-15 15:23:38 +0000

    Created card component

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -17,7 +17,7 @@ export class ApiService {
 
   }
 
-  }
+}
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { ApiService } from './api.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
