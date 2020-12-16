f19015ea9c7a69142c1b525e195064f53e802a65
Scrambles86 <craigvidler86@outlook.com>
2020-12-16 15:58:11 +0000

    Removed erroneous code from api.service file

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,26 +1,14 @@
 import { ApiResponse } from './models/test.model';
-import { Injectable, EventEmitter } from "@angular/core";
+import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-@Injectable({providedIn : 'root'})
-
-
+@Injectable({ providedIn: 'root' })
 export class ApiService {
-
-  apiService = new EventEmitter<ApiService>();
-
   constructor(private http: HttpClient) {}
 
-     getData(): Observable<any> {
-      return this.http.get('https://api.rawg.io/api/games?dates=2010-01-01,2020-12-31&developers=4132');
-        console.log(Game)
-
+  getData(): Observable<object> {
+    return this.http.get(
+      'https://api.rawg.io/api/games?dates=2010-01-01,2020-12-31&developers=4132'
+    );
   }
-
-  }
-
-
-
-
-
-
+}
